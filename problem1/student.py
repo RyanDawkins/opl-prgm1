baab94a8e174aa04f489f94c13e8392aa07f5dc6
Ryan Dawkins <dawkinssu@gmail.com>
2014-10-05 16:23:25 -0500

    Fix

--- problem1/student.py
+++
@@ -1,7 +0,0 @@
-# Base student class for storing information
-class Student:
-    def __init__(self):
-        self.name = None
-        self.age = 0
-        self.gpa = 0.0
-        self.grade = None
