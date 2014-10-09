96e036483ad8243bb47def60027391230f3573f9
Ryan Dawkins <dawkinssu@gmail.com>
2014-10-09 17:08:00 -0500

    Changed the main script to test both methods

--- problem2/problem2.py
+++ problem2/problem2.py
@@ -2,7 +2,18 @@ from quicksort import QuickSort
 
 if __name__ == "__main__":
     sorter = QuickSort()
-    a = [1, 0, 2, 4]
-    print a
-    sorter.iterative(a, 0, len(a)-1)
-    print a
+
+    a0 = [200,133,41,34,2,11]
+    a1 = a0[:] # Creates a copy of a0
+
+    print "Recursive:"
+    print a0
+    sorter.recursive(a0, 0, len(a0)-1)
+    print a0
+
+    print
+
+    print "Iterative:"
+    print a1
+    sorter.iterative(a1, 0, len(a1)-1)
+    print a1
